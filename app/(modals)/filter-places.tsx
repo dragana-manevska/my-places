7c1c560b415c464861cdae735111e4fc013daac9
Dragana Ilievska <106313832+dragana-ilievska@users.noreply.github.com>
2026-03-17 11:15:48 +0100

    chore: update package.json dependencies and scripts
    - Added new script "ios:dev" for easier development on iOS with Expo.
    - Added "expo-file-system" and "expo-media-library" dependencies.
    - Added "expo-sqlite" dependency.
    - Updated "expo-module-scripts" to version 3.0.5.
    - Removed "react-native-maps" dependency.

---
+++ app/(modals)/filter-places.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const FilterPlacesModal = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Filter places</Text>
+    </View>
+  );
+};
+
+export default FilterPlacesModal;
